19f8f75b1b1d9a926e58b99e74bfde0fdf2e36c7
ahmedosama9777 <ahmedosama9777@gmail.com>
2023-01-18 17:06:52 +0200

    Add search if city starts with text

--- search.py
+++ search.py
@@ -1,4 +1,31 @@
-class Search():
-    def search(self, search_text: str):
+class Search:
+    CITIES = [
+        "Paris",
+        "Budapest",
+        "Skopje",
+        "Rotterdam",
+        "Valencia",
+        "Vancouver",
+        "Amsterdam",
+        "Vienna",
+        "Sydney",
+        "New York City",
+        "London",
+        "Bangkok",
+        "Hong Kong",
+        "Dubai",
+        "Rome",
+        "Istanbul",
+    ]
+
+    def search(self, search_text: str) -> list:
         if len(search_text) < 2:
             return []
+
+        search_results: list[str] = []
+
+        for city in self.CITIES:
+            if city.startswith(search_text):
+                search_results.append(city)
+
+        return search_results
--- search_test.py
+++ search_test.py
@@ -2,7 +2,12 @@ from unittest import TestCase
 
 from search import Search
 
+
 class TestSearch(TestCase):
     def test_search_text_less_than_two_chars_returns_no_results(self):
         search_engine = Search()
         self.assertEqual(search_engine.search("r"), [])
+
+    def test_search_returns_city_starting_with_search_text(self):
+        search_engine = Search()
+        self.assertEqual(search_engine.search("Va"), ["Valencia", "Vancouver"])
